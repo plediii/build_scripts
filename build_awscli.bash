f6e539e3114c250e781c78176a27b218546496f1
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2015-02-11 00:25:04 -0600

    Create build_awscli.bash
    Notes on building aws cli.

---
+++ build_awscli.bash
@@ -0,0 +1,6 @@
+
+# From http://docs.aws.amazon.com/cli/latest/userguide/installing.html#install-bundle-other-os
+curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+sudo python get-pip.py
+sudo pip install awscli
+sudo pip install --upgrade awscli
